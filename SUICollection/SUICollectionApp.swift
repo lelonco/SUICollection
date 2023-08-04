19211e94f1f2499af5d311194df2df0d33be8e1b
Yaroslav Ark <fast.yarik@gmail.com>
2023-08-04 20:31:53 +0300

    Initial Commit

---
+++ SUICollection/SUICollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUICollectionApp.swift
+//  SUICollection
+//
+//  Created by Yaroslav on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUICollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
